a0547db5dd53948f7c7c10ff3c8952c82875b7b1
Andrei <mrdaddy@mail.ru>
2018-09-14 11:16:11 +0300

    ssl

--- src/main/java/com/rw/numbered/orders/controllers/BaseController.java
+++ src/main/java/com/rw/numbered/orders/controllers/BaseController.java
@@ -5,6 +5,7 @@ import by.iba.railway.eticket.xml.exception.XmlParserSystemException;
 import com.rw.numbered.orders.dto.ErrorMessage;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.ConstraintViolationException;
+import java.net.ConnectException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @ApiResponses(value = {
         @ApiResponse(code = 400, message = "Bad request", response = ErrorMessage.class, responseContainer = "List"),
-        @ApiResponse(code = 503, message = "Service Unavailable"),
+        @ApiResponse(code = 503, message = "Service Unavailable", response = ErrorMessage.class, responseContainer = "List"),
         @ApiResponse(code = 504, message = "Gateway Timeout")
 })
 @PreAuthorize("hasRole('U')")
@@ -27,7 +30,7 @@ public class BaseController {
     public enum ERROR_PREFIX {validation, system, express}
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<?> handleInvalidRequest(ConstraintViolationException e, WebRequest request) {
-        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        List<ErrorMessage> errors = new ArrayList<>();
         ErrorMessage errorMessage = new ErrorMessage(ERROR_PREFIX.validation+".error",e.getLocalizedMessage());
         errors.add(errorMessage);
         return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
@@ -35,7 +38,7 @@ public class BaseController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleInvalidRequest(MethodArgumentNotValidException e, WebRequest request) {
-        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        List<ErrorMessage> errors = new ArrayList<>();
         if(e.getBindingResult().hasErrors()) {
             for(ObjectError oe: e.getBindingResult().getAllErrors()) {
                 ErrorMessage errorMessage = new ErrorMessage(ERROR_PREFIX.validation+"."+oe.getCodes()[0],oe.getDefaultMessage());
@@ -47,7 +50,7 @@ public class BaseController {
 
     @ExceptionHandler(BusinessSystemException.class)
     public ResponseEntity<?> handleInvalidRequest(BusinessSystemException e, WebRequest request) {
-        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        List<ErrorMessage> errors = new ArrayList<>();
         ErrorMessage errorMessage = new ErrorMessage(ERROR_PREFIX.express+"."+e.getCode(),e.getMessage());
         errors.add(errorMessage);
         return new ResponseEntity(errors, HttpStatus.SERVICE_UNAVAILABLE);
@@ -55,7 +58,7 @@ public class BaseController {
 
     @ExceptionHandler(XmlParserSystemException.class)
     public ResponseEntity<?> handleInvalidRequest(XmlParserSystemException e, WebRequest request) {
-        List<ErrorMessage> errors = new ArrayList<ErrorMessage>();
+        List<ErrorMessage> errors = new ArrayList<>();
         ErrorMessage errorMessage;
         HttpStatus status;
         if("timeout".equals(e.getMessage())) {
@@ -66,4 +69,26 @@ public class BaseController {
             return new ResponseEntity(errors, status);
         }
    }
+
+    @ExceptionHandler(ConnectException.class)
+    protected List<ErrorMessage> handleConnectException(ConnectException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    protected List<ErrorMessage> handleDataAccessException(EmptyResultDataAccessException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
+    }
+
+    @ExceptionHandler(SQLException.class)
+    protected List<ErrorMessage> handleSQLException(SQLException e) {
+        List<ErrorMessage> errors = new ArrayList<>();
+        errors.add(new ErrorMessage(ERROR_PREFIX.system+".database_error", e.getMessage()));
+        return errors;
+    }
+
 }
